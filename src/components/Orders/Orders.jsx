c5183dfba0f58b3e6c01caa1103ba2c314a5ae67
dg1223 <alavi1223@hotmail.com>
2023-05-16 02:58:23 -0400

    debugged cart functionalities

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -11,6 +11,7 @@ const Orders = () => {
 
   const handleRemoveFromCart = (id) => {
     const remaining = cart.filter((product) => product._id !== id);
+    console.log(cart, id);
     setCart(remaining);
     removeFromDb(id);
   };
