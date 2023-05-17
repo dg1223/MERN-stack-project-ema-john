2562e42acdd985f31097395e4d5473a3cff0b934
dg1223 <alavi1223@hotmail.com>
2023-05-17 01:54:54 -0400

    pagination done

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -46,7 +46,9 @@ const Shop = () => {
   // Code given by ChatGPT
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`http://localhost:5000/products`);
+      const res = await fetch(
+        `http://localhost:5000/products?page=${currentPage}&limit=${itemsPerPage}`
+      );
       const data = await res.json();
       setProducts(data);
     };
@@ -55,23 +57,37 @@ const Shop = () => {
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    const savedCart = [];
-    // step 1: get id of the addedProduct
-    for (const id in storedCart) {
-      // step 2: get product from products state by using id
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        // step 3: add quantity
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        // step 4: add the added product to the saved cart
-        savedCart.push(addedProduct);
-      }
-      // console.log('added Product', addedProduct)
-    }
-    // step 5: set the cart
-    setCart(savedCart);
-  }, [products]);
+    const ids = Object.keys(storedCart);
+
+    fetch(`http://localhost:5000/productsById`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(ids),
+    })
+      .then((res) => res.json())
+      .then((cartProducts) => {
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for (const id in storedCart) {
+          // step 2: get product from products state by using id
+          const addedProduct = cartProducts.find(
+            (product) => product._id === id
+          );
+          if (addedProduct) {
+            // step 3: add quantity
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            // step 4: add the added product to the saved cart
+            savedCart.push(addedProduct);
+          }
+          // console.log('added Product', addedProduct)
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+      });
+  }, []);
 
   const handleAddToCart = (product) => {
     // cart.push(product); '
